089bdc1c827ceddcd5cbbaa1902e96830b36dfb9
paul <paul@zhi.com>
2015-06-30 14:43:34 +0800

    add encrypt

---
+++ src/main/java/my/algorithm/Ip2Binary.java
@@ -0,0 +1,7 @@
+package my.algorithm;
+
+/**
+ * Created by paul on 2015/6/30.
+ */
+public class Ip2Binary {
+}
---
+++ src/main/java/my/encrypt/Base64.java
@@ -0,0 +1,7 @@
+package my.encrypt;
+
+/**
+ * Created by paul on 2015/6/30.
+ */
+public class Base64 {
+}
