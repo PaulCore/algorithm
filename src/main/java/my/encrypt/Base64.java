95ebec3c53e6c5333c196d888342580b106ce29a
paul <paul@zhi.com>
2015-07-03 16:05:12 +0800

    encrypt

--- src/main/java/my/encrypt/Base64.java
+++ src/main/java/my/encrypt/Base64.java
@@ -1,7 +1,28 @@
 package my.encrypt;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.IOException;
+
 /**
  * Created by paul on 2015/6/30.
  */
 public class Base64 {
+    public static byte[] decrypt(String key) throws IOException {
+        return new BASE64Decoder().decodeBuffer(key);
+    }
+
+    public static String encrypt(byte[] key) {
+        return new BASE64Encoder().encodeBuffer(key);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String str = "1234";
+        String result1 = Base64.encrypt(str.getBytes());
+        System.out.println("加密后密文为：" + result1);
+
+        byte[] result2 = Base64.decrypt(result1);
+        System.out.println("解密后：" + new String(result2));
+    }
 }
--- src/main/java/my/encrypt/Sha.java
+++ src/main/java/my/encrypt/Sha.java
@@ -1,7 +1,26 @@
 package my.encrypt;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by paul on 2015/6/30.
  */
 public class Sha {
+    public static void process(String text) throws NoSuchAlgorithmException {
+        System.out.println("原始数据：" + text);
+        BigInteger bigInteger = null;
+
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        byte[] inputData = text.getBytes();
+        bigInteger = new BigInteger(md.digest());
+        md.update(inputData);
+        bigInteger = new BigInteger(md.digest());
+        System.out.println("加密后数据：" + bigInteger.toString(16));
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        process("数据123");
+    }
 }
